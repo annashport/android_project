8e336be2cb09b4cea4a3ca416b6ff1a296876873
Mykhailo Shport <mykhailo.shport@globallogic.com>
2025-04-05 15:01:23 +0300

    fixed clubs item is not selected after app startup

--- app/src/main/java/com/example/myyyyapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myyyyapplication/presentation/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(ClubsFragment())
+
         binding.bottomNavigationView.setOnItemSelectedListener{
             when(it.itemId){
                 R.id.clubs -> replaceFragment(ClubsFragment())
@@ -34,7 +34,7 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
-
+        binding.bottomNavigationView.selectedItemId = R.id.clubs
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
