2b860942ff3846e63f92b37f2199c7030edb0795
Mykhailo Shport <mykhailo.shport@globallogic.com>
2025-04-06 15:27:40 +0300

    created custom calendar view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.myyyyapplication"
-        minSdk = 24
-        targetSdk = 35
+        minSdk = 26
         versionCode = 1
         versionName = "1.0"
 
